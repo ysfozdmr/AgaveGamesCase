88f64db0b9f8086338a158427e9b7c8458728962
ysfozdmr <ozdemiryusuf430@gmail.com>
2022-04-30 19:43:33 +0300

    backup

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -8,10 +9,93 @@ public class GameController : MonoBehaviour
     public bool isLevelStart;
     public bool isLevelDone;
     public bool isLevelFail;
+
+    [Header("Tags")]
+    public string TagObstacle;
+    public string TagTurningObstacle;
+    public string TagSwingingObstacle;
+    public string TagChangeBack;
+    public string TagTurningTrigger;
+
+    public int lastLevel;
+    private int isFirstLevelFirstlyCompleted;
+    int tryNum;
+    public int level = 1;
+    public int randomLevelIndex;
+    
+    public static GameController instance;
+   // UIController UI;
+    PlayerController player;
+    private void Awake()
+    {
+        instance = this;
+        lastLevel = PlayerPrefs.GetInt("lastLevel", 1);
+        tryNum = PlayerPrefs.GetInt("tryNum", 1);
+        isFirstLevelFirstlyCompleted = PlayerPrefs.GetInt("firstlevelCompleted", 0);
+    }
     void Start()
     {
-        
+        startMethods();
     }
+    void startMethods()
+    {
+     //   UI = UIController.instance;
+        player = PlayerController.instance;
+    }
+    public void tapToStartAction()
+    {
+        sendLevelStart();
+    }
+    void sendLevelStart()
+    {
+        //UI.isLevelStart = true;
+        //UI.inGamePanel.SetActive(true);
+        isLevelStart = true;
+    }
+    public void completeLevel()
+    {
+       // UI.inGamePanel.SetActive(false);
+      //  UI.completeLevelPanel.SetActive(true);
+        isLevelDone = true;
+       // UI.isLevelDone = true;
+        if (isFirstLevelFirstlyCompleted == 0)
+        {
+            isFirstLevelFirstlyCompleted = 1;
+            PlayerPrefs.SetInt("firstlevelCompleted", isFirstLevelFirstlyCompleted);
+        }
+    }
+    public void levelFail()
+    {
+        isLevelFail = true;
+        //UI.isLevelFail = true;
+    }
+
+    public void EndGameButtonAction()
+    {
+        lastLevel++;
+        PlayerPrefs.SetInt("lastLevel", lastLevel);
+        if (isLevelDone)
+        {
+
+            if (PlayerPrefs.GetInt("Level") < 3)
+            {
+                PlayerPrefs.SetInt("Level", (PlayerPrefs.GetInt("Level") + 1));
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+            }
+
+            else
+            {
+                PlayerPrefs.SetInt("Level", 0);
+                SceneManager.LoadScene(PlayerPrefs.GetInt("Level"));
+            }
+        }
+
+
+    }
+
+
+  
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,20 @@ public class PlayerController : MonoBehaviour
     [Header("Bools")] public bool isLevelStart;
     public bool isLevelDone;
     public bool isLevelFail;
+    [Header("Tags")] string TagObstacle;
+    string TagTurningObstacle;
+    string TagSwingingObstacle;
+    string TagChangeBack;
+    string TagTurningTrigger;
 
     [Header("Movement Settings")] public float movementSpeed;
     Animator playerAnimCont;
     SplineFollower splineFollower;
 
+    [Header("Camera Settings")] public CinemachineVirtualCamera turningObjectCam;
+    public CinemachineVirtualCamera swingingObjectCam;
+    public List<GameObject> FailingCubes = new List<GameObject>();
+    private int index;
 
     GameController GC;
 
@@ -31,9 +41,23 @@ public class PlayerController : MonoBehaviour
 
     void StartMethods()
     {
+        GC = GameController.instance;
         splineFollower = GetComponent<SplineFollower>();
         playerAnimCont = GetComponent<Animator>();
         splineFollower.followSpeed = movementSpeed;
+
+        GetTags();
+    }
+
+  
+
+    void GetTags()
+    {
+        TagObstacle = GC.TagObstacle;
+        TagChangeBack = GC.TagChangeBack;
+        TagSwingingObstacle = GC.TagSwingingObstacle;
+        TagTurningObstacle = GC.TagTurningObstacle;
+        TagTurningTrigger = GC.TagTurningTrigger;
     }
 
     // Update is called once per frame
@@ -42,16 +66,63 @@ public class PlayerController : MonoBehaviour
         Movement();
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(TagObstacle))
+        {
+            playerAnimCont.enabled = false;
+            StartCoroutine(FailingCor());
+        }
+
+        if (other.gameObject.CompareTag(TagTurningObstacle))
+        {
+            turningObjectCam.Priority = 11;
+        }
+
+        if (other.gameObject.CompareTag(TagSwingingObstacle))
+        {
+            swingingObjectCam.Priority = 11;
+        }
+
+        if (other.gameObject.CompareTag(TagChangeBack))
+        {
+            swingingObjectCam.Priority = 9;
+            turningObjectCam.Priority = 9;
+            index++;
+        }
+    }
+
+    public void splinechanger()
+    {
+        splineFollower.Restart(0.19f);
+    }
+    IEnumerator FailingCor()
+    {
+        yield return new WaitForSeconds(1.5f);
+        if (index == 0)
+        {
+            playerAnimCont.enabled = true;
+            transform.position = FailingCubes[0].gameObject.transform.position;
+            splineFollower.Restart(0.19f);
+        }
+        else if (index == 1)
+        {
+            playerAnimCont.enabled = true;
+            transform.position = FailingCubes[1].gameObject.transform.position;
+        }
+    }
+    
     void Movement()
     {
         if (Input.GetMouseButton(0))
         {
             playerAnimCont.SetBool("isRunning", true);
             splineFollower.enabled = true;
+            
         }
         else
         {
-            playerAnimCont.SetBool("isRunning",false);
+            playerAnimCont.SetBool("isRunning", false);
             splineFollower.enabled = false;
         }
     }
